045a0f90dfe36719a437cee44c1133ab878d5688
Jakub Ďuraš <jakub@duras.me>
2018-08-18 00:50:42 +0200

    Add prebuilt support and win32 prebuilt

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var leftClicky = require('./build/Release/left-clicky.node');
+// Will load a compiled build if present or a prebuild.
+// If no build if found it will throw an exception
+var leftClicky = require('node-gyp-build')(__dirname)
 
 module.exports = leftClicky;
